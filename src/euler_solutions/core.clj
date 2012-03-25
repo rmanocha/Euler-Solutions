7ae77d168113dc35b648106dcc8ae4cb44f873ff
Rishabh Manocha <rmanocha@gmail.com>
2012-03-25 14:33:08 -0400

    Solution to problem 1 - passed

--- src/euler_solutions/core.clj
+++ src/euler_solutions/core.clj
@@ -1,12 +1,13 @@
 (ns euler-solutions.core)
 
-(defn problem-1 []
-  (let [mul-sum 0]
-    (for [index (range 1000)]
+(defn problem-1 [n]
+  (loop [nn 0 mul-sum 0]
+    (if (= n nn)
+      mul-sum
       (cond
-        (= 0 (mod index 3)) (+ mul-sum index)
-        (= 0 (mod index 5)) (+ mul-sum index)))
-    mul-sum))
+        (= 0 (mod nn 3)) (recur (inc nn) (+ nn mul-sum))
+        (= 0 (mod nn 5)) (recur (inc nn) (+ nn mul-sum))
+        :else (recur (inc nn) mul-sum)))))
 
 (defn -main [& args]
-  (println (problem-1)))
+  (println (problem-1 1000)))
