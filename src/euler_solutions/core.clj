82c591dc54892d9bad69ea3f462a401eb7e4798d
Rishabh Manocha <rmanocha@gmail.com>
2012-03-25 16:31:28 -0400

    Finishing up problem 2

--- src/euler_solutions/core.clj
+++ src/euler_solutions/core.clj
@@ -9,5 +9,14 @@
         (= 0 (mod nn 5)) (recur (inc nn) (+ nn mul-sum))
         :else (recur (inc nn) mul-sum)))))
 
+; This comes from http://icyrock.com/blog/2011/07/solving-project-euler-problem-2-in-clojure/
+(defn fibs
+  ([] (fibs 1 2))
+  ([a b] (lazy-seq (cons a (fibs b (+ a b))))))
+
+(defn problem-2 [n]
+  (reduce + (filter #(= 0 (mod % 2)) (take-while (partial > n) (fibs)))))
+
 (defn -main [& args]
-  (println (problem-1 1000)))
+  (println (problem-1 1000))
+  (println (problem-2 4e6)))
